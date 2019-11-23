b737e00ad9e3171a8fd961c4f178bc348ff3ea6b
Chris Milson <chris.w.milson@gmail.com>
2019-11-23 10:48:09 +0900

    Write basic boilerplate
    By adapting code from Brian Schwind (https://blog.bschwind.com/) I made
    a small test script to make sure that libraries work and I will be able
    to read the input from the reciever module.

---
+++ main.py
@@ -0,0 +1,47 @@
+"""
+  This code is adapted from Camillo Addis's code on 
+  https://medium.com/@camilloaddis/smart-air-conditioner-with-raspberry-pi-an-odissey-2a5b438fe984
+"""
+
+import RPi.GPIO as GPIO
+from datetime import datetime
+
+PIN = 16
+
+SHORT, MEDIUM, LONG, LONGEST = 600, 1500, 24000, 30000
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN, GPIO.IN)
+
+val = 1
+
+while True:
+  while val:
+    val = GPIO.input(PIN)
+  
+  start = datetime.now()
+  ones = 0
+  prev = 0
+  command = []
+
+  while True:
+    if val != prev:
+      now = datetime.now()
+      pulse = now - start
+      start = now
+      command.append((prev, pulse.microseconds))
+
+    if ones > 10000: break
+
+    if val: ones += 1
+    else: ones = 0
+
+    prev = val
+    val = GPIO.input(PIN)
+
+  print("----------------START----------------")
+
+  for (val, length) in command:
+    print(f"Value {val}, length {length}")
+
+  print("-----------------END-----------------")
