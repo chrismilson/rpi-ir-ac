3c759d7fd185f32108850a5ac7b6c23e64672d6f
Chris Milson <chris.w.milson@gmail.com>
2019-11-24 14:48:30 +0900

    Uncomment GPIO references

--- transmit.py
+++ transmit.py
@@ -1,21 +1,21 @@
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 from time import sleep
 from sys import argv
 
-# PIN = 15 # GPIO 22
+PIN = 15 # GPIO 22
 SHORT, LONG = int(argv[1]), int(argv[2])
 
 SHORT /= 1000000
 LONG /= 1000000
 
-# # GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 
-# # GPIO.setup(PIN, GPIO.OUT)
+GPIO.setup(PIN, GPIO.OUT)
 
 def txBit(bit):
-  # # GPIO.output(PIN, GPIO.HIGH)
+  GPIO.output(PIN, GPIO.HIGH)
   sleep(SHORT)
-  # # GPIO.output(PIN, GPIO.LOW)
+  GPIO.output(PIN, GPIO.LOW)
   sleep(SHORT * bit + LONG * (bit ^ 1))
 
 data = [
@@ -31,17 +31,17 @@ data = [
   0xFF
 ]
 
-# # GPIO.output(PIN, GPIO.HIGH)
+GPIO.output(PIN, GPIO.HIGH)
 sleep(.03)
-# # GPIO.output(PIN, GPIO.LOW)
+GPIO.output(PIN, GPIO.LOW)
 sleep(.05)
-# # GPIO.output(PIN, GPIO.HIGH)
+GPIO.output(PIN, GPIO.HIGH)
 sleep(.03)
-# # GPIO.output(PIN, GPIO.LOW)
+GPIO.output(PIN, GPIO.LOW)
 sleep(.02)
 
 for val in data:
   for i in range(7, 1, -1):
     txBit((val >> i) & 1)
 
-# GPIO.cleanup()
+GPIO.cleanup()
