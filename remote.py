888a1c653e372ea44dd50fbbf50f47bb96e417b5
Chris Milson <chris.w.milson@gmail.com>
2019-11-25 14:48:09 +0900

    Increase indent

--- remote.py
+++ remote.py
@@ -12,20 +12,20 @@ class Remote:
 
   def getConf(self):
     s =  "begin remote\n"
-    s += "name  {}\n".format(self.name)
-    s += "flags RAW_CODES\n"
-    s += "eps   30\n"
-    s += "aeps  100\n"
-    s += "begin raw_codes\n"
+    s += "  name  {}\n".format(self.name)
+    s += "  flags RAW_CODES\n"
+    s += "  eps   30\n"
+    s += "  aeps  100\n"
+    s += "  begin raw_codes\n"
     for command in self.commands:
-      s += "  name {}\n".format(command.name)
+      s += "    name {}\n".format(command.name)
       s += "\n".join(wrap(
         command.pulseSpace(self.SHORT, self.LONG),
         80,
-        initial_indent="    ",
-        subsequent_indent="    "
+        initial_indent="      ",
+        subsequent_indent="      "
       )) + "{}\n".format(self.LONG)
-    s += "end raw_codes\n"
-    s += "end remote\n"
+    s += "  end raw_codes\n"
+    s += "  end remote\n"
 
     return s
